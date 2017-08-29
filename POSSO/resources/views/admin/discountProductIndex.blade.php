72a3b2b5d93cd56fc81275e336ab27ccb5b0f93c
clifforddlewis <mrmiket95@gmail.com>
2017-08-29 10:36:49 +0700

    discounted product view

---
+++ POSSO/resources/views/admin/discountProductIndex.blade.php
@@ -0,0 +1,40 @@
+@extends('admin/admin-layout')
+@section('breadcrumbs')
+<li><a href="{{url('admin/index')}}">Admin</a></li>
+<li class="active">Product</li>
+@stop
+@section('box-title')
+Discounted Product
+@stop
+@section('box-body')
+
+<table class="table table-hover table-striped table-bordered table-condensed" id="dataTable">
+	<thead>
+		<tr>
+			<td>#</td>
+			<td>Nama Product</td>
+			<td>Harga</td>
+			<td>Designer</td>
+			<td>Action</td>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($data as $index => $value)
+		<tr>
+			<td>{{$index+1}}</td>
+			<td><a href="{{url('admin/product/detailed',$value['id'])}}">{{$value['nama_product']}}</a></td>
+			<td>{{number_format($value['harga_product'])}}</td>
+			<td>{{$value['designer_product']}}</td>
+			<td>
+			<a href="{{url('admin/product/edit',$value['id'])}}"><button class="btn btn-flat btn-warning">Ubah</button></a>
+			@if($value['status_product']==0)
+			<a href="{{url('admin/product/enable',$value['id'])}}"><button class="btn btn-flat btn-success">Enable</button></a>
+			@else
+			<a href="{{url('admin/product/disable',$value['id'])}}"><button class="btn btn-flat btn-danger">Disable</button></a>
+			@endif
+			</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+@stop
