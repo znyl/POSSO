0d738c02f2e4b135d2996a2cf009a7588bd5827b
clifforddlewis <mrmiket95@gmail.com>
2017-10-05 02:37:24 +0700

    table size created

---
+++ POSSO/database/migrations/2017_10_05_013935_create_table_size.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSize extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sizes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('nama_size');
+            $table->integer('lingkar_dada');
+            $table->integer('lingkar_pinggul');
+            $table->integer('panjang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sizes');
+    }
+}
