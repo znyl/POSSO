3f7931b9f2a86cb60d3f006fa201497e79f969be
clifforddlewis <mrmiket95@gmail.com>
2017-10-05 02:38:58 +0700

    new controller for size

---
+++ POSSO/app/Http/Controllers/productSizeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\product;
+use App\size;
+use Illuminate\Http\Request;
+
+class productSizeController extends Controller
+{
+    //
+    public function insert(Request $request)
+    {
+    	$insert = new size;
+        $insert->lingkar_dada = $request->lingkar_dada;
+        $insert->lingkar_pinggul = $request->lingkar_pinggul;
+        $insert->panjang = $request->panjang;
+        $insert->product_id = $request->product_id;
+        if($insert->save())
+        {
+            return redirect()->action('productController@detailed',$request->id)->with('success','Ukuran berhasil ditambahkan');
+        }
+    }
+    public function edit($id)
+    {
+    	$data = size::find($id);
+    	return view('admin.productSizeEditForm',compact('data'));
+    }
+    public function update(Request $request)
+    {
+    	$insert = size::find($request->id)
+        $insert->lingkar_dada = $request->lingkar_dada;
+        $insert->lingkar_pinggul = $request->lingkar_pinggul;
+        $insert->panjang = $request->panjang;
+        if($insert->save())
+        {
+            return redirect()->action('productController@detailed',$request->id)->with('success','Ukuran berhasil diubah');
+        }
+    }
+}
