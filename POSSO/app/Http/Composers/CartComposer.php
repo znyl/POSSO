c6ddec437b05357e796bd4d98fc1746972b0e32e
clifforddlewis <mrmiket95@gmail.com>
2017-12-16 03:42:30 +0700

    new cartcomposer file

---
+++ POSSO/app/Http/Composers/CartComposer.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Composers;
+use Illuminate\Contracts\View\View;
+class CartComposer  {
+
+	public function compose($view)
+	{
+		$cart = array();
+        $total=0;
+        $index=0;
+        $item=0;
+        if(session('shopping_cart')!=null)
+        {
+            foreach(session('shopping_cart') as $tipe => $value)
+            {
+                foreach ($value as $product_id => $value2) 
+                {
+                    $get=\App\product::find($product_id);
+                    foreach($value2 as $size => $qty)
+                    {
+                    	$get['gambar']=\App\file_gambar::find($get['file_gambar_id']);
+                        $get['size']=\App\size::find($size);
+                        $get['qty']=$qty;
+                        $get['tipe']=$tipe;
+                        if($tipe=="Beli")
+                            $tipediskon= 1;
+                        else if($tipe=="Sewa")
+                            $tipediskon=2;
+                        if($get->discount->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->count()>0)
+                        {
+                            $get['diskon'] = \App\discount::where('product_id',$get->id)->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->first();
+                            
+                            $get['diskon_status']=true;
+                        }
+                        else
+                            $get['diskon_status']=false;
+
+                        if($get['diskon_status'])
+                           	$total+= $get['diskon']->harga_diskon * $get['qty'];
+                        else
+                            $total+= $get->harga_product * $get['qty'];
+
+                        
+
+                        $item++;
+                        array_push($cart,$get);
+                    }   
+                }
+               
+            }   
+        }
+        $view->with(compact('cart','total','item'));
+	}
+}
