1429711bb5c2f97b98f511018075c1a206719542
clifforddlewis <mrmiket95@gmail.com>
2017-12-16 03:47:12 +0700

    navbar cart worked

--- POSSO/resources/views/front/layoutCart.blade.php
+++ POSSO/resources/views/front/layoutCart.blade.php
@@ -1,23 +1,34 @@
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" >
+    <a href="{{url('/cart')}}" class="dropdown-toggle" data-toggle="dropdown" >
         <i class="fa fa-shopping-bag"></i>
-        <span class="badge" id="cart-badge">0</span>
+        <span class="badge" id="cart-badge">{{$item}}</span>
     </a>
     <ul class="dropdown-menu cart-list" id="cart-item">
+        @if(session('shopping_cart')==null)
         <li>
             Keranjang Belanja Kosong
         </li>
+        @endif
         <!-- 
         
         -->
-       <li>
-            <a href="#" class="photo"><img src="/image/product/DSC_0116.jpg" class="cart-thumb" alt="" /></a>
-            <h6><a href="#">Delica omtantur </a></h6>
-            <p class="m-top-10">2x - <span class="price">$99.99</span></p>
+        @foreach($cart as $index => $value)
+        <li>
+            <a href="{{url('/product/detailed',$value['id'])}}" class="photo"><img src="{{asset($value['gambar']->direktori_file)}}" class="cart-thumb" alt="" /></a>
+            <h6><a href="{{url('/product/detailed',$value['id'])}}">{{$value['nama_product']}} </a></h6>
+            <p class="m-top-10">{{$value['qty']}}x - <span class="price">
+            @if($value['diskon_status'])
+            Rp. {{number_format($value['diskon']->harga_diskon,0,",",".")}}
+            @else
+            Rp. {{number_format($value['harga_product'],0,",",".")}}
+            @endif
+            </span></p>
         </li>
+        
+         @endforeach
         <!---- More List ---->
         <li class="total">
-            <span class="pull-right" id="total"><strong>Total</strong>: Rp.0</span>
+            <span class="pull-right" id="total"><strong>Total</strong>: Rp.{{number_format($total,0,",",".")}}</span>
             <a href="{{url('/cart')}}" class="btn btn-cart">Cart</a>
         </li>
     </ul>
