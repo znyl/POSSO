fbef5033386a44465c3573a6b49f75346e0a133f
clifforddlewis <mrmiket95@gmail.com>
2017-12-16 03:42:52 +0700

    removing cart function

--- POSSO/app/Http/Controllers/cartController.php
+++ POSSO/app/Http/Controllers/cartController.php
@@ -13,30 +13,8 @@ class cartController extends Controller
     //
     public function index()
     {
-    	
-    	$cart = array();
-        $index=0;
-    	if(session('shopping_cart')!=null)
-    	{
-	    	foreach(session('shopping_cart') as $tipe => $value)
-	        {
-	            foreach ($value as $product_id => $value2) 
-                {
-                    $get=product::find($product_id);
-                    foreach($value2 as $size => $qty)
-                    {
-                        $get['size']=size::find($size);
-                        $get['qty']=$qty;
-                        $get['tipe']=$tipe;
-                        array_push($cart,$get);
-                    }   
-                }
-               
-	        }	
-    	}
-	    	
-        
-    	return view('front.cartIndex',compact('cart'));
+    		
+    	return view('front.cartIndex');
     }
     public function addCart(Request $request)
     {
