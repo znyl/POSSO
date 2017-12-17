a1fb87256d1ce33a80669b1f38a80319999ab63b
clifforddlewis <mrmiket95@gmail.com>
2017-12-18 00:42:28 +0700

    social media button
    add button linked to social media
    
    remove the bottom part of the footer

--- POSSO/resources/views/front/layoutFooter.blade.php
+++ POSSO/resources/views/front/layoutFooter.blade.php
@@ -3,8 +3,15 @@
         <div class="row">
             <div class="main_company roomy-100 text-center">
                 <h3 class="text-uppercase">Possobelenzo.</h3>
-                <p>7th floor - Palace Building - 221b Walk of Fame - London- United Kingdom</p>
-                <p>(+84). 123. 456. 789  -  info@poiseidon.lnk</p>
+                <p>Citraland - Surabaya Barat</p>
+                <p>081331508450 / WhatsApp: 081234502820</p>
+                <p>
+                    <a href=""><button class="btn btn-small"><i class="fa fa-facebook"></i></button></a>
+                    <a href=""><button class="btn btn-small"><i class="fa fa-instagram"></i></button></a>
+                    <a href=""><button class="btn btn-small"><i class="fa fa-twitter"></i></button></a>
+                    <a href=""><button class="btn btn-small"><i class="fa fa-pinterest"></i></button></a>
+                </p>
+                <p> <a target="_blank" href="http://bootstrapthemes.co"><i class="fa fa-heart"></i></a> </p>
             </div>
         </div>
     </div>
@@ -17,28 +24,3 @@
 </div><!-- End off scroll up -->
 
 
-<footer id="footer" class="footer bg-mega">
-    <div class="container">
-        <div class="row">
-            <div class="main_footer p-top-40 p-bottom-30">
-                <div class="col-md-6 text-left sm-text-center">
-                    <p class="wow fadeInRight" data-wow-duration="1s">
-                        Made with 
-                        <i class="fa fa-heart"></i>
-                        by 
-                        <a target="_blank" href="http://bootstrapthemes.co">Bootstrap Themes</a> 
-                        2016. All Rights Reserved
-                    </p>
-                </div>
-                <div class="col-md-6 text-right sm-text-center sm-m-top-20">
-                    <ul class="list-inline">
-                        <li><a href="">Facebook</a></li>
-                        <li><a href="">Twitter</a></li>
-                        <li><a href="">Instagram</a></li>
-                        <li><a href="">Pinterest</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
