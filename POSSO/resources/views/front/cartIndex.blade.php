a15c82ad0deb2669b7c536e39ff361a9b9961b4f
clifforddlewis <mrmiket95@gmail.com>
2017-12-18 00:41:00 +0700

    add checkout button and linked to form

--- POSSO/resources/views/front/cartIndex.blade.php
+++ POSSO/resources/views/front/cartIndex.blade.php
@@ -9,52 +9,52 @@ Shopping Cart
 model-banner
 @stop
 @section('content')
-<section class="margin-top-120" style="margin-top:120px;">
+<section class="margin-top-120">
 	<div class="container">
 		<div class="table-responsive" >
-		<table class="table table-border">
-			<thead>
-				<tr>
-					<td class="col-md-4" colspan="2">Product</td>
-					<td class="col-md-1">Ukuran</td>
-					<td class="col-md-1">Tipe Transaksi</td>
-					<td>Qty.</td>
-					<td>Harga</td>
-					<td>Total</td>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($cart as $index => $value)
-				<tr>
-				<td><a href="#" class="photo"><img src="{{asset($value['gambar']->direktori_file)}}" class="cart-image"></a></td>
-				<td>
-				{{$value['nama_product']}}
-				</td>
-				</td>
-				<td>{{$value['size']->nama_size}}</td>
-				<td>{{$value['tipe']}}</td>
-				<td> 
-					<form action="/refreshCart" method="post">
-						{{csrf_field()}}
-						<input type="hidden" name="product_id" value="{{$value['id']}}">
-						<input type="hidden" name="size" value="{{$value['size']->id}}">
-						<input type="hidden" name="tipe" value="{{$value['tipe']}}">
-						<div class="col-md-6 col-sm-6 col-xs-6">
-							<input name="qty" type="number" class="form-control" value="{{$value['qty']}}"  required>
-						</div>
+			<table class="table table-border">
+				<thead>
+					<tr>
+						<td class="col-md-4" colspan="2">Product</td>
+						<td class="col-md-1">Ukuran</td>
+						<td class="col-md-1">Tipe Transaksi</td>
+						<td>Qty.</td>
+						<td>Harga</td>
+						<td>Total</td>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($cart as $index => $value)
+					<tr>
+					<td><a href="#" class="photo"><img src="{{asset($value['gambar']->direktori_file)}}" class="cart-image"></a></td>
+					<td>
+					{{$value['nama_product']}}
+					</td>
+					</td>
+					<td>{{$value['size']->nama_size}}</td>
+					<td>{{$value['tipe']}}</td>
+					<td> 
+						<form action="/refreshCart" method="post">
+							{{csrf_field()}}
+							<input type="hidden" name="product_id" value="{{$value['id']}}">
+							<input type="hidden" name="size" value="{{$value['size']->id}}">
+							<input type="hidden" name="tipe" value="{{$value['tipe']}}">
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<input name="qty" type="number" class="form-control" value="{{$value['qty']}}"  required>
+							</div>
 
-						<button class="btn btn-small"><i class="fa fa-refresh"></i></button>
-						<button class="btn btn-small" type="button"><i class="fa fa-trash"></i></button>
-					</form>
+							<button class="btn btn-small"><i class="fa fa-refresh"></i></button>
+							<button class="btn btn-small" type="button"><i class="fa fa-trash"></i></button>
+						</form>
+						
+					</td>
+					<td>Rp. @if($value['diskon_status']) {{number_format($value['diskon']->harga_diskon,0,",",".")}} @else {{number_format($value['harga_product'],0,",",".")}} @endif</td>
+					<td>Rp. @if($value['diskon_status']) {{number_format($value['diskon']->harga_diskon*$value['qty'],0,",",".")}} @else {{number_format($value['harga_product']*$value['qty'],0,",",".")}} @endif</td>
+					</tr>
+					@endforeach
 					
-				</td>
-				<td>Rp. @if($value['diskon_status']) {{number_format($value['diskon']->harga_diskon,0,",",".")}} @else {{number_format($value['harga_product'],0,",",".")}} @endif</td>
-				<td>Rp. @if($value['diskon_status']) {{number_format($value['diskon']->harga_diskon*$value['qty'],0,",",".")}} @else {{number_format($value['harga_product']*$value['qty'],0,",",".")}} @endif</td>
-				</tr>
-				@endforeach
-				
-			</tbody>
-		</table>
+				</tbody>
+			</table>
 		<div class="row">
 			<div class="col-md-4">
 				<table class="table table-bordered">
@@ -69,7 +69,7 @@ model-banner
 				</table>
 			</div>
 		</div>
-		<a href=""><button class="btn btn-default">Check Out!</button></a>
+		<a href="{{url('/checkout')}}"><button class="btn btn-default">Check Out!</button></a>
 		<hr>
 	</div>
 </section>
