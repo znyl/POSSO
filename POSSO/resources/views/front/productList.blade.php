912cecfb03e7c28228fcd94d7e837c1b7582fe0c
clifforddlewis <mrmiket95@gmail.com>
2017-12-21 00:41:14 +0700

    nothing

--- POSSO/resources/views/front/productList.blade.php
+++ POSSO/resources/views/front/productList.blade.php
@@ -17,7 +17,8 @@ model-banner
 							<img src="{{asset($value['main_image']['direktori_file'])}}" width="100%" class="img-fluid product-image">
 							<p class="product-title">{{$value['nama_product']}}</p>
 							<p class="product-designer">By. {{$value['designer_product']}}</p>
-							<p class="product-price @if($value['status_diskon']) line-through @endif">Rp. {{number_format($value['harga_product'],0,",",".")}} @if($value['status_diskon']) <span class="badge">{{$value['diskon']['discount']}}% Off</span></p> @endif
+
+							<p class="product-price @if($value['status_diskon']) line-through @endif">Rp. {{number_format($value['harga_product'],0,",",".")}} @if($value['status_diskon']) <span class="badge">{{$value['diskon']['discount']}} % OFF</span></p> @endif
 							<p class="product-price">&nbsp;@if($value['status_diskon'])  Rp. {{number_format($value['diskon']['harga_diskon'],0,",",".")}} @endif</p>
 						</div>
 					</div>
