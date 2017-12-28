b186e677b9908f02f90adc8927f3675da487a463
clifforddlewis <mrmiket95@gmail.com>
2017-12-28 10:23:16 +0800

    set relation for order_detail

--- POSSO/app/order.php
+++ POSSO/app/order.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     //
-    
+    public function order_detail()
+    {
+    	return $this->hasMany('App\order_detail','order_id');
+    }
 }
