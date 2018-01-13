b1a589dccac88a7794184a129b6407ba3767f798
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:30:53 +0700

    create colors table

---
+++ POSSO/database/migrations/2018_01_08_225849_create_table_color.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableColor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->char('nama_warna',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colors');
+    }
+}
