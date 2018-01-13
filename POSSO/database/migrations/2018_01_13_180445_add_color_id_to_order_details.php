1fc4ebc74a69f0e340451c17b7b89189286608fc
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:32:12 +0700

    add foreign key to order_detail
    color

---
+++ POSSO/database/migrations/2018_01_13_180445_add_color_id_to_order_details.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColorIdToOrderDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+            $table->integer('color_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+            $table->dropColumn('color_id');
+        });
+    }
+}
