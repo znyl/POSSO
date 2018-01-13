1e6fb89572d2c772921e198be69dd0ad182e8855
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:28:14 +0700

    add color index to cart

--- POSSO/app/Http/Composers/CartComposer.php
+++ POSSO/app/Http/Composers/CartComposer.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\View\View;
 class CartComposer  {
 
 	public function compose($view)
-	{
+	{session()->flush();
 		$cart = array();
         $total=0;
         $index=0;
@@ -15,46 +15,51 @@ class CartComposer  {
             {
                 foreach ($value as $product_id => $value2) 
                 {
-                    $get=\App\product::find($product_id);
-                    foreach($value2 as $size => $qty)
+                    foreach($value2 as $warna => $value3)
                     {
-                    	$get['gambar']=\App\file_gambar::find($get['file_gambar_id']);
-                        $get['size']=\App\size::find($size);
-                        $get['qty']=$qty['qty'];
-                        $get['tipe']=$tipe;
-                        if($tipe=="Beli")
-                            $tipediskon= 1;
-                        else if($tipe=="Sewa")
+                        $get=\App\product::find($product_id);
+                        foreach($value3 as $size => $qty)
                         {
-                            $tipediskon=2;
-                            $get['tgl_mulai']=$qty['tgl_mulai'];
-                            $get['tgl_akhir']=$qty['tgl_akhir'];
+                            $get['gambar']=\App\file_gambar::find($get['file_gambar_id']);
+                            $get['size']=\App\size::find($size);
+                            $get['warna'] = \App\color::find($warna);
+                            $get['qty']=$qty['qty'];
+                            $get['tipe']=$tipe;
+                            if($tipe=="Beli")
+                                $tipediskon= 1;
+                            else if($tipe=="Sewa")
+                            {
+                                $tipediskon=2;
+                                $get['tgl_mulai']=$qty['tgl_mulai'];
+                                $get['tgl_akhir']=$qty['tgl_akhir'];
+                                
+                            }
+                            if($get->discount->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->count()>0)
+                            {
+                                $get['diskon'] = \App\discount::where('product_id',$get->id)->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->first();
+                                
+                                $get['diskon_status']=true;
+                            }
+                            else
+                                $get['diskon_status']=false;
+
+                            if($get['diskon_status'])
+                                $total+= $get['diskon']->harga_diskon * $get['qty'];
+                            else
+                            {
+                                if($tipe == "Beli")
+                                    $total+= $get->harga_product * $get['qty'];
+                                else if ($tipe=="Sewa")
+                                    $total+= $get->harga_sewa_product*$get['qty'];
+                            }
+
                             
-                        }
-                        if($get->discount->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->count()>0)
-                        {
-                            $get['diskon'] = \App\discount::where('product_id',$get->id)->where('tgl_mulai','<=',date('Y-m-d'))->where('tgl_akhir','>=',date('Y-m-d'))->where('tipe_transaksi',$tipediskon)->first();
-                            
-                            $get['diskon_status']=true;
-                        }
-                        else
-                            $get['diskon_status']=false;
 
-                        if($get['diskon_status'])
-                           	$total+= $get['diskon']->harga_diskon * $get['qty'];
-                        else
-                        {
-                        	if($tipe == "Beli")
-                            	$total+= $get->harga_product * $get['qty'];
-                            else if ($tipe=="Sewa")
-                            	$total+= $get->harga_sewa_product*$get['qty'];
-                        }
-
-                        
-
-                        $item++;
-                        array_push($cart,$get);
-                    }   
+                            $item++;
+                            array_push($cart,$get);
+                        } 
+                    }
+                      
                 }
                
             }   
--- POSSO/app/Http/Controllers/cartController.php
+++ POSSO/app/Http/Controllers/cartController.php
@@ -19,7 +19,6 @@ class cartController extends Controller
     }
     public function addCart(Request $request)
     {
-
         $data = session('shopping_cart');
         if($data==null)
         {
@@ -28,20 +27,22 @@ class cartController extends Controller
         }
         
         if($request->submit == "Beli")
-            $data[$request->submit][$request->product_id][$request->size]['qty']=$request->qty;
+            $data[$request->submit][$request->product_id][$request->warna][$request->size]['qty']=$request->qty;
         else if ($request->submit == "Sewa")
         {
-            $data[$request->submit][$request->product_id][$request->size]['tgl_mulai']=$request->tgl_mulai;
-            $data[$request->submit][$request->product_id][$request->size]['tgl_akhir']=$request->tgl_akhir;
-            $data[$request->submit][$request->product_id][$request->size]['qty']= (strtotime($request->tgl_akhir) - strtotime($request->tgl_mulai))/86400;
+            $data[$request->submit][$request->product_id][$request->warna][$request->size]['tgl_mulai']=$request->tgl_mulai;
+            $data[$request->submit][$request->product_id][$request->warna][$request->size]['tgl_akhir']=$request->tgl_akhir;
+            $data[$request->submit][$request->product_id][$request->warna][$request->size]['durasi']= (strtotime($request->tgl_akhir) - strtotime($request->tgl_mulai))/86400;
+            $data[$request->submit][$request->product_id][$request->warna][$request->size]['qty'] = $request->qty;
+
             
 
         }
         
-        if($data[$request->submit][$request->product_id][$request->size]==0)
+        if($data[$request->submit][$request->product_id][$request->warna][$request->size]['qty']==0)
         {
 
-            unset($data[$request->submit][$request->product_id][$request->size]);
+            unset($data[$request->submit][$request->product_id][$request->warna][$request->size]);
         }
         session(['shopping_cart'=>$data]);
         if($request->qty<1&&$request->submit=="Beli")
@@ -62,27 +63,29 @@ class cartController extends Controller
         }
         
         if($request->tipe=="Beli")
-            $data[$request->tipe][$request->product_id][$request->size]['qty']=$request->qty;
+            $data[$request->tipe][$request->product_id][$request->warna][$request->size]['qty']=$request->qty;
         else if($request->tipe="Sewa")
         {
-            $data[$request->tipe][$request->product_id][$request->size]['tgl_mulai']=$request->tgl_mulai;
-            $data[$request->tipe][$request->product_id][$request->size]['tgl_akhir']=$request->tgl_akhir;
-            $data[$request->tipe][$request->product_id][$request->size]['qty']= (strtotime($request->tgl_akhir) - strtotime($request->tgl_mulai))/86400;   
+            $data[$request->tipe][$request->product_id][$request->warna][$request->size]['tgl_mulai']=$request->tgl_mulai;
+            $data[$request->tipe][$request->product_id][$request->warna][$request->size]['tgl_akhir']=$request->tgl_akhir;
+            $data[$request->tipe][$request->product_id][$request->warna][$request->size]['durasi']= (strtotime($request->tgl_akhir) - strtotime($request->tgl_mulai))/86400;
+            $data[$request->tipe][$request->product_id][$request->warna][$request->size]['qty']=$request->qty;
+
         }
         print_r($data);
-        if($data[$request->tipe][$request->product_id][$request->size]==0)
+        if($data[$request->tipe][$request->product_id][$request->warna][$request->size]['qty']=$request->qty==0)
         {
 
-            unset($data[$request->tipe][$request->product_id][$request->size]);
+            unset($data[$request->tipe][$request->product_id][$request->warna][$request->size]);
         }
         session(['shopping_cart'=>$data]);
         return redirect('/cart');
     }
 
-    public function deleteCart($tipe, $product_id, $size)
+    public function deleteCart($tipe, $product_id, $size, $warna)
     {
         $data = session('shopping_cart');
-        unset($data[$tipe][$product_id][$size]);
+        unset($data[$request->tipe][$request->product_id][$request->warna][$request->size]);
         session(['shopping_cart'=>$data]);
         return redirect('/cart');
     }
