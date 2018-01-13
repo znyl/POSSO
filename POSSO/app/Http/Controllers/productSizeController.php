4455bdcdd177f2abacb24133856ffee72f101170
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:30:24 +0700

    delete and status function

--- POSSO/app/Http/Controllers/productSizeController.php
+++ POSSO/app/Http/Controllers/productSizeController.php
@@ -16,6 +16,7 @@ class productSizeController extends Controller
         $insert->panjang = $request->panjang;
         $insert->nama_size = $request->nama_size;
         $insert->product_id = $request->product_id;
+        $insert->status=1;
         if($insert->save())
         {
             return redirect()->action('productController@detailed',$request->product_id)->with('success','Ukuran berhasil ditambahkan');
@@ -37,5 +38,29 @@ class productSizeController extends Controller
         {
             return redirect()->action('productController@detailed',$insert->product_id)->with('success','Ukuran berhasil diubah');
         }
+    }
+    public function setStatus($id)
+    {
+        $data = size::find($id);
+        if($data->status==1)
+        {
+            if(size::where('status','=',1)->where('product_id','=',$data->product_id)->count()>0 && $data->product->status==1)
+                $data->status=0;
+            else
+                return redirect()->action('productController@detailed',$data->product_id)->with('error','Silahkan mengaktifkan Ukuran lainnya terlebih dahulu');
+
+        }
+        else
+            $data->status=1;
+        $data->save();
+        return redirect()->action('productController@detailed',$data->product_id)->with('success','Status berhasil diubah');
+    }
+    public function delete($id)
+    {
+        $data = size::find($id);
+        if($data->order_detail->count()>0)
+            return redirect()->action('productController@detailed',$data->product_id)->with('error','Data tidak dapat dihapus');
+        $data->destroy();
+        return redirect()->action('productController@detailed',$data->product_id)->with('success','Data berhasil dihapus');
     }
 }
