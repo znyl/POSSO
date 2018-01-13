f2621857b07f625b371f7000a4c364819c06bb23
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:28:37 +0700

    custom order detailed

--- POSSO/app/Http/Controllers/customOrderController.php
+++ POSSO/app/Http/Controllers/customOrderController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\custom_order;
 
 class customOrderController extends Controller
 {
     //
     public function index()
     {
-    	
+    	$data = custom_order::where('status','!=','0')->where('status','<','4')->get();
+    	return view('admin.orderCustomIndex',compact('data'));
+    }
+    public function detailed($id)
+    {
+    	$data = custom_order::find($id);
+    	return view('admin.orderCustomDetailed',compact('data'));
     }
 }
