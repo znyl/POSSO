33feb5ddfc0a2850aeaba94806f32480fe6ebe28
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:32:22 +0700

    small fix

--- POSSO/resources/views/admin/admin-layout.blade.php
+++ POSSO/resources/views/admin/admin-layout.blade.php
@@ -140,14 +140,14 @@
                 <div class="alert alert-success alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
                 <h4><i class="icon fa fa-check"></i>Success!</h4>
-                {{session('success')}}
+                {!! session('success') !!}
                 </div>
             @endif
             @if(session('error'))
                 <div class="alert alert-danger alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
                 <h4><i class="icon fa fa-ban"></i>Error!</h4>
-                {{!! session('error') !!}}
+                {!! session('error') !!}
                 </div>
             @endif
             @if(session('warning'))
