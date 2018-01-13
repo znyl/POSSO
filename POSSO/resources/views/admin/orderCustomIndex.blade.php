d7f88e477f90eeea4f62535029e4d56164ae3439
clifforddlewis <mrmiket95@gmail.com>
2018-01-13 18:32:46 +0700

    custom order view

---
+++ POSSO/resources/views/admin/orderCustomIndex.blade.php
@@ -0,0 +1,51 @@
+@extends('admin/admin-layout')
+@section('breadcrumbs')
+<li><a href="{{url('admin/index')}}">Admin</a></li>
+<li class="active">Custom Order</li>
+@stop
+@section('box-title')
+Custom Order
+@stop
+@section('box-body')
+<table class="table table-condensed table-striped table-hover table-bordered" id="dataTable">
+	<thead>
+		<tr>
+			<td>#</td>
+			<td>Kode Invoice</td>
+			<td>Nama Konsumen</td>
+			<td>E - Mail</td>
+			<td>Status</td>
+			<td>Tgl. Order</td>
+			<td>Budget</td>
+			<td>Action</td>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($data as $index => $value)
+		<tr>
+			<td>{{$index+1}}</td>
+			<td><a href="{{url('admin/orderCustom/detailed',$value['id'])}}">{{$value['kode_order']}}</a></td>
+			<td>{{$value['nama_konsumen']}}</td>
+			<td>{{$value['email_konsumen']}}</td>
+
+			@if($value['status']==1)
+			<td>Belum Diproses</td>
+			@elseif($value['status']==2)
+			<td>Telah Dikonfirmasi</td>
+			@elseif($value['status']==3)
+			<td>Telah Dikirim</td>
+			@elseif($value['status']==4)
+			<td>Order telah selesai</td>
+			@elseif($value['status']==0)
+			<td>Order dibatalkan</td>
+			@endif
+			<td>{{$value['created_at']}}</td>
+			<td>< {{number_format($value['budget'])}}</td>
+			<td></td>
+
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+@stop
+
